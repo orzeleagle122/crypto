9864bbcf92f8a8019aaf398ed8a7b62163e205c4
Patryk Orłowski <orzeleagle122@gmail.com>
2021-10-22 11:35:58 +0200

    crypto details

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,13 +1,13 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': 'c0b70ff419mshf6abc2a144dfa76p19dbe1jsn4a93647205b3',
-}
+};
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com'
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
@@ -16,7 +16,13 @@ export const cryptoApi = createApi({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`),
         }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({coinId, timePeriod}) => createRequest(`/coin/${coinId}/history/${timePeriod}`),
+        }),
     }),
-})
+});
 
-export const {useGetCryptosQuery} = cryptoApi
+export const {useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} = cryptoApi;
